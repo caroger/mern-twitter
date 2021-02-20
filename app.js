cae9fe99f6dfa6a28044efe2bbc4fb54d72092f2
caroger <rogerhu1989@gmail.com>
2021-02-19 23:22:58 -0800

    started working on user auth

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const express = require("express");
 const app = express();
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
+const port = process.env.PORT || 5000;
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MangoDB successfully"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello Roger"));
-
-const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Hello Roger");
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
