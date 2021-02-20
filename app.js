4404368a0e5ee8087514b995f0b62e36ad6274d8
caroger <rogerhu1989@gmail.com>
2021-02-20 13:01:23 -0800

    finished user model and registration route

--- app.js
+++ app.js
@@ -4,15 +4,28 @@ const express = require("express");
 const app = express();
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+// for post man
+const bodyParser = require("body-parser");
 
+// looking for ports in production env
 const port = process.env.PORT || 5000;
+
+// connect to db
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MangoDB successfully"))
   .catch((err) => console.log(err));
 
+// enable POSTMAN testing
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello Roger");
+  res.send("Hello World");
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
