a138604e20216c9794c50ae860307ccf12065f36
caroger <rogerhu1989@gmail.com>
2021-02-20 23:36:14 -0800

    reducers

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>A Twitter Clone</h1>
+        <footer>Copyright &Copyright 2021 Roger</footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
